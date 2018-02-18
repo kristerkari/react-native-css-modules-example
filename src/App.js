a0f7cab131ec3ef7f44a5a6e0458a0509f90f38d
krister <krister@sent.at>
2018-02-18 21:37:12 +0200

    Wrap App with SafeAreaView

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { ScrollView } from "react-native";
+import { SafeAreaView, ScrollView } from "react-native";
 import { ProfileCard } from "./ProfileCard";
 import { Buttons } from "./Buttons";
 import { Link } from "./Link";
@@ -8,18 +8,20 @@ import styles from "./App.css";
 export default class App extends Component {
   render() {
     return (
-      <ScrollView className={styles.wrapper}>
-        <Link
-          description="Profile card from"
-          url="https://themes.getbootstrap.com/products/application"
-        />
-        <ProfileCard />
-        <Link
-          description="Buttons from"
-          url="https://codepen.io/coolzilj/pen/ImlEG"
-        />
-        <Buttons />
-      </ScrollView>
+      <SafeAreaView style={{ flex: 1 }}>
+        <ScrollView className={styles.wrapper}>
+          <Link
+            description="Profile card from"
+            url="https://themes.getbootstrap.com/products/application"
+          />
+          <ProfileCard />
+          <Link
+            description="Buttons from"
+            url="https://codepen.io/coolzilj/pen/ImlEG"
+          />
+          <Buttons />
+        </ScrollView>
+      </SafeAreaView>
     );
   }
 }
