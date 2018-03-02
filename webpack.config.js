5a00db99f4a3d72d2006036a75edf3d36a2b6bd6
krister <krister@sent.at>
2018-03-03 00:03:47 +0200

    Update to Webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js?$/,
         exclude: /node_modules/,
