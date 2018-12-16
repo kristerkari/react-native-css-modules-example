b68f62aa63e7f5c1fba26363a6b57b59a38357aa
krister <krister@sent.at>
2018-12-16 12:55:23 +0200

    allow jsx extension

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: "babel-loader",
         query: {
