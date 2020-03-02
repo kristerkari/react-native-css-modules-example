767a7872240dd75cfd9910ded3d6057942a2eed7
krister <krister@sent.at>
2020-03-02 16:02:03 -0300

    Fix ESLint warnings

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,7 @@ const {getDefaultConfig} = require('metro-config');
 
 module.exports = (async () => {
   const {
-    resolver: {sourceExts, assetExts},
+    resolver: {sourceExts},
   } = await getDefaultConfig();
   return {
     transformer: {
